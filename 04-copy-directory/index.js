f1c06d354f606ad3811f96ff850df0be4dc0a049
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-05-05 18:19:14 +0300

    fix: fix folder copy update when files of original folder were deleted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,14 @@
 const PATH = require('path');
-const FS = require('fs/promises');
+const FS = require('fs');
+const FS_PROMISES = require('fs/promises');
+const { error } = require('console');
 
 const coped_folder_name = 'files';
 const clone_folder_name = `${coped_folder_name}-copy`;
 
 async function copyFolder(coped_folder_parent, clone_folder_parent, coped_folder_name, clone_folder_name) {
   const coped_folder_path = PATH.join(coped_folder_parent, coped_folder_name);
-  const file_dirents = await FS.readdir(
+  const file_dirents = await FS_PROMISES.readdir(
     coped_folder_path,
     {
       encoding: 'utf8',
@@ -15,7 +17,8 @@ async function copyFolder(coped_folder_parent, clone_folder_parent, coped_folder
   );
   const clone_folder_path = PATH.join(clone_folder_parent, clone_folder_name);
 
-  await FS.mkdir(clone_folder_path, { recursive: true });
+  await FS_PROMISES.rm(clone_folder_path, { recursive: true, force: true });
+  await FS_PROMISES.mkdir(clone_folder_path);
 
   let coped_file_path;
   let clone_file_path;
@@ -26,9 +29,17 @@ async function copyFolder(coped_folder_parent, clone_folder_parent, coped_folder
     } else {
       coped_file_path = PATH.join(coped_folder_path, item.name);
       clone_file_path = PATH.join(clone_folder_path, item.name);
-      FS.copyFile(coped_file_path, clone_file_path);
+      FS_PROMISES.copyFile(coped_file_path, clone_file_path);
     }
   });
 }
+
+// async function removeDir(path) {
+//   FS.access(path, FS.constants.F_OK, error => {
+//     if(error) {
+//       FS_PROMISES.re
+//     }
+//   });
+// }
 
 copyFolder(__dirname, __dirname, coped_folder_name, clone_folder_name);
